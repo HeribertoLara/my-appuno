b684274de0607397273a9c10dcc5f0bc19a92f66
unknown <ing.quimico.heriberto.lara@gmil.com>
2020-07-17 16:07:17 -0500

    Updates

---
+++ precache-manifest.c738437d0825b8549c7894deb1177de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e9d37cb461a403b7eb91da14363d43",
+    "url": "/my-appuno/index.html"
+  },
+  {
+    "revision": "bf9d455413a2d37878c1",
+    "url": "/my-appuno/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8ab23d1e2d9cacd428ca",
+    "url": "/my-appuno/static/js/2.6610a979.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-appuno/static/js/2.6610a979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf9d455413a2d37878c1",
+    "url": "/my-appuno/static/js/main.584f23b4.chunk.js"
+  },
+  {
+    "revision": "1416417dc1a188549a66",
+    "url": "/my-appuno/static/js/runtime-main.66fbc82d.js"
+  }
+]);
